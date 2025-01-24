fccb2e47c5e3b214921f45dbd2b6db8077c4c56a
MarkFantini <marcelo.asfantini@gmail.com>
2025-01-23 22:15:28 -0300

    Exercise 3.8

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token("body", (request) => JSON.stringify(request.body))
+
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :body'
+  )
+)
 
 let persons = [
   { 
@@ -85,10 +92,6 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person)
 })
-
-app.use(morgan.token('type', (request, response) => {
-  return request.headers['content-type']
-}))
 
 const PORT = 3001
 app.listen(PORT, () => {
