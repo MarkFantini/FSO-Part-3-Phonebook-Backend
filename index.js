f16be49742e7ac174be398d8f686e17d691bc67c
MarkFantini <marcelo.asfantini@gmail.com>
2025-02-11 18:53:38 -0300

    Created errorHandler middleware at the end of index.js

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ app.get('/', (request, response) => {
 })
 
 
+
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons)
@@ -56,15 +57,8 @@ app.get('/api/persons', (request, response) => {
 })
 
 
-app.get('/api/persons/:id', (request, response) => {
-  // const id = request.params.id
-  // const person = persons.find(person => person.id === id)
 
-  // if (person) {
-  //     response.json(person)
-  // } else {
-  //     response.status(404).end()
-  // }
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
@@ -76,15 +70,7 @@ app.get('/api/persons/:id', (request, response) => {
     .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  console.log('ID to be deleted: ', id)
-  persons = persons.filter(person => person.id !== id)
-  console.log('Persons after deletion')
-  console.log(persons)
 
-  response.status(204).end()
-})
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -112,6 +98,8 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
+
+
 app.put('/api/persons/:id', (request, response) => {
   const body = request.body
   const name = body.name
@@ -134,6 +122,40 @@ app.put('/api/persons/:id', (request, response) => {
 
   response.json(updatedPerson)
 })
+
+
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  console.log('ID to be deleted: ', id)
+  persons = persons.filter(person => person.id !== id)
+  console.log('Persons after deletion')
+  console.log(persons)
+
+  response.status(204).end()
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
